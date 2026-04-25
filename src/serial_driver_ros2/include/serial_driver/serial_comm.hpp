781adc81d789d76e87f886f1fe1938f0f92f1362
qyw23AI <3082128164@qq.com>
2026-04-25 16:09:10 +0800

    直接上传C语言串口通信

---
+++ src/serial_driver_ros2/include/serial_driver/serial_comm.hpp
@@ -0,0 +1,27 @@
+#ifndef SERIAL_COMM_HPP
+#define SERIAL_COMM_HPP
+
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <serial/serial.h>
+
+class SerialComm {
+public:
+    SerialComm(const std::string& port, unsigned long baudrate);
+    ~SerialComm();
+
+    // send
+    bool sendFloatArrayCommand(const std::vector<float>& values);
+    // receive
+    std::vector<float> readFloatArrayResponse();
+
+
+private:
+    serial::Serial serial_port_;
+
+    bool isOpen();
+    std::vector<uint8_t> encodeFloatArray(const std::vector<float>& values);
+};
+
+#endif
